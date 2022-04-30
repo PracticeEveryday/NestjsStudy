a2c2b1a9ea0a28c2678b49a0ffc9828c25bd58de
DongHyun Kim <developerforus@gmail.com>
2022-04-30 22:34:37 +0900

    router

--- chapter1/letsStart/dist/app.js
+++ chapter1/letsStart/dist/app.js
@@ -5,7 +5,16 @@ var app_model_1 = require("./app.model");
 var app = express();
 var data = [1, 2, 3, 4];
 app.get("/", function (req, res) {
+    console.log(req.rawHeaders);
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res) {
+    console.log(req.rawHeaders[1]);
+    res.send({ cats: app_model_1.Cat, blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    console.log(req.rawHeaders[1]);
+    res.send({ som: app_model_1.Cat[1] });
 });
 app.listen(8000, function () {
     console.log("server on..");
--- chapter1/letsStart/src/app.ts
+++ chapter1/letsStart/src/app.ts
@@ -9,9 +9,25 @@ const data: number[] = [1, 2, 3, 4];
 
 // front나 client가 endpoint에서 겟을 요청 했을 때 그에 대한 응답으로 db의 정보를 가져다가 준것!!
 app.get("/", (req: express.Request, res: express.Response) => {
+  // 요청이 어디서 왔는가 찍어보자
+  console.log(req.rawHeaders);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req: express.Request, res: express.Response) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat, blue: Cat[0] });
+});
+
+// 라우터 => cl font가 http get으로 endpoint로 넘겨주면
+// back이 코드를 실행하며 데이터를 가공 해 res.send로 넘겨줌!!
+app.get("/cats/som", (req: express.Request, res: express.Response) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ som: Cat[1] });
 });
 
 app.listen(8000, () => {
   console.log("server on..");
 });
+
+// middleware 양쪽을 연결하여 데이트를 주고 받을 수 있도록 중간에서 매개 역할을 하는 것
