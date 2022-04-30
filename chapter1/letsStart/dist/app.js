7263e9d66d61a2fa309bf968acf4551265707235
DongHyun Kim <developerforus@gmail.com>
2022-04-30 22:41:13 +0900

    middleware

--- chapter1/letsStart/dist/app.js
+++ chapter1/letsStart/dist/app.js
@@ -4,19 +4,35 @@ var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
 var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
+app.get("/cats/som", function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is som middleware");
+    next();
+});
 app.get("/", function (req, res) {
-    console.log(req.rawHeaders);
     res.send({ cats: app_model_1.Cat });
 });
 app.get("/cats/blue", function (req, res) {
-    console.log(req.rawHeaders[1]);
     res.send({ cats: app_model_1.Cat, blue: app_model_1.Cat[0] });
 });
 app.get("/cats/som", function (req, res) {
-    console.log(req.rawHeaders[1]);
     res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res) {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found" });
 });
 app.listen(8000, function () {
     console.log("server on..");
+});
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is middleware");
+    next();
 });
 //# sourceMappingURL=app.js.map
--- chapter1/letsStart/src/app.ts
+++ chapter1/letsStart/src/app.ts
@@ -7,23 +7,44 @@ const app: express.Express = express();
 
 const data: number[] = [1, 2, 3, 4];
 
+// next 다음 라우터로 이동하게 해주는 함수
+// middleware 거치고 해당하는 router찾음
+// use = middleware
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  // next() 필수!!
+  next();
+});
+
+// cats/blue는 안찍힘
+app.get("/cats/som", (req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is som middleware");
+  // next() 필수!!
+  next();
+});
+
 // front나 client가 endpoint에서 겟을 요청 했을 때 그에 대한 응답으로 db의 정보를 가져다가 준것!!
 app.get("/", (req: express.Request, res: express.Response) => {
   // 요청이 어디서 왔는가 찍어보자
-  console.log(req.rawHeaders);
   res.send({ cats: Cat });
 });
 
 app.get("/cats/blue", (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
   res.send({ cats: Cat, blue: Cat[0] });
 });
 
 // 라우터 => cl font가 http get으로 endpoint로 넘겨주면
-// back이 코드를 실행하며 데이터를 가공 해 res.send로 넘겨줌!!
+// back이 db 에서 데이터 가져 온 후 코드(비즈니스 로직)를 실행하며 데이터를 가공 해 res.send로 넘겨줌!!
 app.get("/cats/som", (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
   res.send({ som: Cat[1] });
+});
+
+// error 처리 미들웨어가 될 수 있음..
+app.use((req, res) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found" });
 });
 
 app.listen(8000, () => {
@@ -31,3 +52,11 @@ app.listen(8000, () => {
 });
 
 // middleware 양쪽을 연결하여 데이트를 주고 받을 수 있도록 중간에서 매개 역할을 하는 것
+
+// 밑에 있으면 /cats/som 찾고 끝내버리기 때문에 middleware안나옴 so 내가 주고 싶은 곳에 적절하게 middleware 줄 수 있음.
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware");
+  // next() 필수!!
+  next();
+});
