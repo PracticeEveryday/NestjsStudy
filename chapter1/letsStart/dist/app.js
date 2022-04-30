a21a7f184b408ab578d5283ffa946f3558e2430c
DongHyun Kim <developerforus@gmail.com>
2022-04-30 23:20:05 +0900

    single pattern

--- chapter1/letsStart/dist/app.js
+++ chapter1/letsStart/dist/app.js
@@ -2,25 +2,38 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cats_route_1 = require("./cats/cats.route");
-var app = express();
-var data = [1, 2, 3, 4];
-app.use(function (req, res, next) {
-    console.log(req.rawHeaders[1]);
-    console.log("this is logging middleware");
-    next();
-});
-app.use(express.json());
-app.use(cats_route_1.default);
-app.use(function (req, res) {
-    console.log("this is error middleware");
-    res.send({ error: "404 not found" });
-});
-app.listen(8000, function () {
-    console.log("server on..");
-});
-app.use(function (req, res, next) {
-    console.log(req.rawHeaders[1]);
-    console.log("this is middleware");
-    next();
-});
+var Server = (function () {
+    function Server() {
+        var app = express();
+        this.app = app;
+    }
+    Server.prototype.setRouter = function () {
+        this.app.use(cats_route_1.default);
+    };
+    Server.prototype.setMiddleware = function () {
+        this.app.use(function (req, res, next) {
+            console.log(req.rawHeaders[1]);
+            console.log("this is logging middleware");
+            next();
+        });
+        this.app.use(express.json());
+        this.setRouter();
+        this.app.use(function (req, res) {
+            console.log("this is error middleware");
+            res.send({ error: "404 not found" });
+        });
+    };
+    Server.prototype.listen = function () {
+        this.setMiddleware();
+        this.app.listen(8000, function () {
+            console.log("server on..");
+        });
+    };
+    return Server;
+}());
+function init() {
+    var server = new Server();
+    server.listen();
+}
+init();
 //# sourceMappingURL=app.js.map
--- chapter1/letsStart/src/app.ts
+++ chapter1/letsStart/src/app.ts
@@ -3,42 +3,64 @@
 import * as express from "express";
 // cat이라는 데이터 베이스에서 가져왔다고 생각할 것
 import catsRouter from "./cats/cats.route";
-const app: express.Express = express();
 
-const data: number[] = [1, 2, 3, 4];
+// app의 싱글톤 패턴 도입 클래스로 하나의 인스턴스만 만들기!! 태초에 한 번만 만들기에 메모리 낭비 방지 가능
+// 인스턴스 하나 찍힐 때 app이 하나 생성될 것임
+class Server {
+  public app: express.Application;
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-// next 다음 라우터로 이동하게 해주는 함수
-// middleware 거치고 해당하는 router찾음
-// use = middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("this is logging middleware");
-  // next() 필수!!
-  next();
-});
+  private setRouter() {
+    this.app.use(catsRouter);
+  }
+  private setMiddleware() {
+    // next 다음 라우터로 이동하게 해주는 함수
+    // middleware 거치고 해당하는 router찾음
+    // use = middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("this is logging middleware");
+      // next() 필수!!
+      next();
+    });
 
-// json middleware
-// express 에서 제공하는 body값 읽을 수 있게 하는 미들웨어
-app.use(express.json());
-app.use(catsRouter);
+    // json middleware
+    // express 에서 제공하는 body값 읽을 수 있게 하는 미들웨어
+    this.app.use(express.json());
 
-// error 처리 미들웨어가 될 수 있음..
-// 404 middleware
-app.use((req, res) => {
-  console.log("this is error middleware");
-  res.send({ error: "404 not found" });
-});
+    this.setRouter();
 
-app.listen(8000, () => {
-  console.log("server on..");
-});
+    // error 처리 미들웨어가 될 수 있음..
+    // 404 middleware
+    this.app.use((req, res) => {
+      console.log("this is error middleware");
+      res.send({ error: "404 not found" });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log("server on..");
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+init();
 
 // middleware 양쪽을 연결하여 데이트를 주고 받을 수 있도록 중간에서 매개 역할을 하는 것
 
 // 밑에 있으면 /cats/som 찾고 끝내버리기 때문에 middleware안나옴 so 내가 주고 싶은 곳에 적절하게 middleware 줄 수 있음.
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("this is middleware");
-  // next() 필수!!
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.rawHeaders[1]);
+//   console.log("this is middleware");
+//   // next() 필수!!
+//   next();
+// });
